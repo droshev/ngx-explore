00860b11a476f236924d058fd86ebc56be5a517e
droshev <mladen.droshev@sap.com>
2021-12-06 17:18:27 -0500

    add rating

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ngx-explore';
+  config = {
+    indicatorCapacity: 5,
+    allowHalves: false,
+    disabled: false,
+    size: 'md',
+  };
+  sizes = ['xs', 'sm', 'md', 'lg', 'cozy', 'compact', 'condensed'];
+  modelValue = 2;
+  totalRatings:number = 5;
+  value:number = 3.34;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { CdkTableModule } from '@angular/cdk/table';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+
+import { RatingIndicatorModule } from '@fundamental-ngx/core/rating-indicator';
+import { ButtonModule } from '@fundamental-ngx/core/button';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    CdkTableModule,
+    DragDropModule,
+    ButtonModule,
+    RatingIndicatorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
